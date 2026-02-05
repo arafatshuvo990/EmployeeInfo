262c831525bb7e86200fdf5e4408b8e72dde8a38
arafatshuvo990 <arafatshuvoworks@outlook.com>
2026-02-05 19:48:51 +0600

    git ignore updated

---
+++ EmployeeApi/Mappers/EmployeeFamilyMapper.cs
@@ -0,0 +1,6 @@
+﻿namespace EmployeeApi.Mappers
+{
+    public class EmployeeFamilyMapper
+    {
+    }
+}
--- EmployeeApi/obj/Debug/net9.0/EmployeeApi.AssemblyInfo.cs
+++ EmployeeApi/obj/Debug/net9.0/EmployeeApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4d35a96ac6486a5853e6163b9cb7878d06f6e9d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18d99ca4c8bb00eea84f4e20aa75b7fd6c4cb82a")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeeApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
